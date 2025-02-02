a47adc15c00b7f959cde9b9628348179669dab17
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2025-02-02 19:46:22 -0300

    criando endpoints

--- src/main/java/com/br/mesttra/Main.java
+++ src/main/java/com/br/mesttra/Main.java
@@ -1,7 +1,134 @@
 package com.br.mesttra;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.br.mesttra.Models.Produto;
+import com.google.gson.Gson;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
 public class Main {
+    private static List<Produto> lista_produtos = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Spark.port(8081);
+
+        //Lidando com o CORS
+        Spark.options("/*", new Route() {
+            @Override
+            public Object handle(Request requisicaoHttp, Response respostaHttp) throws Exception {
+
+                String accessControlRequestHeaders = requisicaoHttp.headers("Access-Control-Request-Headers");
+                
+                if (accessControlRequestHeaders != null) 
+                    respostaHttp.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+                
+                String accessControlRequestMethod = requisicaoHttp.headers("Access-Control-Request-Method");
+
+                if (accessControlRequestMethod != null) 
+                    respostaHttp.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+                
+                return "OK";
+            }
+        });
+
+        //Informando o Browser que é aceito os metodos HTTP OPTIONS, GET, POST, PUT, DELETE para qualquer endereço
+        Spark.before(new spark.Filter() {
+            @Override
+            public void handle(Request requisicaoHttp, Response respostaHttp) throws Exception {
+                respostaHttp.header("Access-Control-Allow-Origin", "*"); // Permite todas as origens
+                respostaHttp.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+            }
+        });
+
+        Spark.get("/listar", listar_produtos());
+        Spark.post("/cadastrar", cadastrar_produto());
+        Spark.put("/atualizar/:codigo", atualizar_produto());
+        Spark.delete("/deletar/:codigo", deletar_produto());
     }
+
+    private static Route listar_produtos() {
+        return (Request Request, Response response) -> {
+            response.type("application/json");
+            response.status(200);
+            return new Gson().toJson(lista_produtos);
+        };
+    }
+
+    private static Route cadastrar_produto() {
+        return (Request request, Response response) -> {
+            Produto novo_produto = new Gson().fromJson(request.body(), Produto.class);
+            novo_produto.setId(lista_produtos.size() + 1);
+            lista_produtos.add(novo_produto);
+
+            response.type("application/json");
+            response.status(201);
+            return new Gson().toJson(novo_produto);
+        };
+    }
+
+    private static Route atualizar_produto() {
+        return (Request request, Response response) -> {
+            int codigo = Integer.parseInt(request.params(":codigo"));         
+
+            if (lista_produtos.isEmpty()) {
+                response.status(404); 
+                return new Gson().toJson("Não existem contatos na base.");
+
+            } else {
+                for(Produto produto : lista_produtos){
+                    if (produto.getId()  == codigo) {
+
+                        // Converte o JSON recebido no corpo da requisição para um objeto Contato
+                        Produto produto_atualizado = new Gson().fromJson(request.body(), Produto.class);
+
+                        // Atualiza os dados do contato
+
+                        produto.setNome_produto(produto_atualizado.getNome_produto());
+                        produto.setCategoria_produto(produto_atualizado.getCategoria_produto());
+                        produto.setPreco_produto(produto_atualizado.getPreco_produto());
+ 
+                        response.type("application/json");
+
+                        response.status(200);
+
+                        return new Gson().toJson("Usuário com ID " + codigo + " foi atualizado com sucesso!");
+                    }
+                   
+                }
+
+                response.status(404); // 404 Not Found
+                return new Gson().toJson("Contato com o ID " + codigo + " especificado não encontrado.");
+            }
+        };
+    }
+
+    private static Route deletar_produto() {
+        return (Request request, Response response) -> {
+            int codigo = Integer.parseInt(request.params(":codigo"));
+            
+            // Lógica de exclusão
+            if (lista_produtos.isEmpty()) {
+                response.status(404); // 404 Not Found
+                return new Gson().toJson("Não existem contatos na base");
+            } 
+
+            boolean produto_removido = lista_produtos.removeIf(pessoa ->
+            pessoa.getId() == codigo);
+
+            if (produto_removido) {
+                response.type("application/json"); // Define o tipo de conteúdo como JSON
+
+                response.status(200);
+                return new Gson().toJson("Usuário com ID " + codigo + " foi excluído com sucesso!");
+            } else {
+                response.status(404);
+                return new Gson().toJson("Contato com ID " + codigo + " não encontrado.");
+            }
+        };
+    }
+    
 }
